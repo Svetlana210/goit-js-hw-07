0b7c53b184343ead3927a0f1cd7935ae62a0d88a
Svetlana210 <svetik.may24@gmail.com>
2022-11-08 11:52:34 +0100

    add gallery1

---
+++ js/01-gallery.js
@@ -0,0 +1,54 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+// console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+function createGallery() {
+    return galleryItems.map(({preview, original, description}) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    }).join('');
+}
+// console.log(createGallery())
+
+
+gallery.insertAdjacentHTML('beforeend', createGallery());
+
+
+gallery.addEventListener('click', onImageClick);
+
+function onImageClick(e) {
+  e.preventDefault();
+  if (e.target === e.currentTarget)
+    return;
+  
+  const selectedImage = e.target.dataset.source;
+  // console.log(selectedImage)
+  
+
+const instance = basicLightbox.create(`
+    <img src="${selectedImage}" width="800" height="600">
+`)
+
+  instance.show()
+  
+  window.addEventListener('keydown', onEscToClose)
+  
+  function onEscToClose(e) {
+    if (e.key === 'Escape') {
+    instance.close()
+  }
+}
+}
+
+
